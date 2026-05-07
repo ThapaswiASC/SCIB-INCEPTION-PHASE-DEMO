45178c4102bd1c87719db9dde667e5d50da0cdd8
ThapaswiASC <ThapaswiASC@users.noreply.github.com>
2026-05-07 14:22:30 +0530

    feat: add custom exception classes

---
+++ code/src/main/java/com/myproject/exceptions/DatabaseConnectionException.java
@@ -0,0 +1,12 @@
+package com.myproject.exceptions;
+
+public class DatabaseConnectionException extends RuntimeException {
+
+    public DatabaseConnectionException(String message) {
+        super(message);
+    }
+
+    public DatabaseConnectionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
