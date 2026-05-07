eba668bc7a9cba7fe56b662d4496c3ab32923d04
ThapaswiASC <ThapaswiASC@users.noreply.github.com>
2026-05-07 17:09:31 +0530

    fix: Add single-parameter constructor to DatabaseConnectionException

--- code/src/main/java/com/myproject/exceptions/DatabaseConnectionException.java
+++ code/src/main/java/com/myproject/exceptions/DatabaseConnectionException.java
@@ -3,6 +3,11 @@ package com.myproject.exceptions;
 public class DatabaseConnectionException extends RuntimeException {
     private final Object preservedInput;
 
+    public DatabaseConnectionException(String message) {
+        super(message);
+        this.preservedInput = null;
+    }
+
     public DatabaseConnectionException(String message, Object preservedInput) {
         super(message);
         this.preservedInput = preservedInput;
