8fc936e1add27311dbea7361912f3e5dccb68764
ThapaswiASC <ThapaswiASC@users.noreply.github.com>
2026-05-07 12:54:42 +0530

    feat: add Column controller for Kanban operations

--- code/src/main/java/com/myproject/controllers/ColumnController.java
+++ code/src/main/java/com/myproject/controllers/ColumnController.java
@@ -5,24 +5,24 @@ import com.myproject.models.dtos.BulkUpdateColumnCountsResponse;
 import com.myproject.models.dtos.ColumnStatsResponse;
 import com.myproject.services.interfaces.ColumnService;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/v1/columns")
-@RequiredArgsConstructor
+@RequestMapping("/v1")
 public class ColumnController {
-    
-    private final ColumnService columnService;
-    
-    @GetMapping("/{columnId}/stats")
-    public ResponseEntity<ColumnStatsResponse> getColumnStats(@PathVariable String columnId) {
+
+    @Autowired
+    private ColumnService columnService;
+
+    @GetMapping("/columns/{columnId}/stats")
+    public ResponseEntity<ColumnStatsResponse> getColumnStatistics(@PathVariable String columnId) {
         ColumnStatsResponse response = columnService.getColumnStats(columnId);
         return ResponseEntity.ok(response);
     }
-    
-    @PutMapping("/bulk-update")
+
+    @PutMapping("/columns/bulk-update")
     public ResponseEntity<BulkUpdateColumnCountsResponse> bulkUpdateColumnCounts(
             @Valid @RequestBody BulkUpdateColumnCountsRequest request) {
         BulkUpdateColumnCountsResponse response = columnService.bulkUpdateColumnCounts(request);
