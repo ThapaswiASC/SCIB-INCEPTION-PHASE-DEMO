95f446099e118031ac564554a8ad43eebdf9bbb7
ThapaswiASC <ThapaswiASC@users.noreply.github.com>
2026-05-08 15:32:31 +0530

    fix: remove duplicate constructor in ColumnNotFoundException

--- code/src/main/java/com/myproject/exceptions/ColumnNotFoundException.java
+++ code/src/main/java/com/myproject/exceptions/ColumnNotFoundException.java
@@ -5,8 +5,4 @@ public class ColumnNotFoundException extends RuntimeException {
     public ColumnNotFoundException(String columnId) {
         super(String.format("Column with ID %s not found", columnId));
     }
-
-    public ColumnNotFoundException(String message) {
-        super(message);
-    }
 }
