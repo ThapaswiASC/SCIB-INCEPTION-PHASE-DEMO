78621cafae236c6e0a54813cdb45e89caf1baae4
ThapaswiASC <ThapaswiASC@users.noreply.github.com>
2026-05-12 12:24:16 +0530

    fix: Add missing methods to ColumnService interface

--- code/src/main/java/com/myproject/services/interfaces/ColumnService.java
+++ code/src/main/java/com/myproject/services/interfaces/ColumnService.java
@@ -1,8 +1,17 @@
 package com.myproject.services.interfaces;
 
+import com.myproject.models.dtos.BulkUpdateColumnCountsRequest;
+import com.myproject.models.dtos.BulkUpdateColumnCountsResponse;
+import com.myproject.models.dtos.ColumnStatsResponse;
 import com.myproject.models.dtos.ColumnTaskCountResponse;
 
 public interface ColumnService {
 
     ColumnTaskCountResponse getColumnTaskCount(String columnId);
+    
+    ColumnStatsResponse getColumnStats(String columnId);
+    
+    BulkUpdateColumnCountsResponse bulkUpdateColumnCounts(BulkUpdateColumnCountsRequest request);
+    
+    void updateColumnCounts(String oldColumnId, String newColumnId);
 }
