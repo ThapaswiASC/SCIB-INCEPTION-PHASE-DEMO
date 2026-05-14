41fda9809480b1448c40a1374da088cb1cb9ecc1
ThapaswiASC <ThapaswiASC@users.noreply.github.com>
2026-05-14 10:29:10 +0530

    feat: add service interfaces

--- code/src/main/java/com/myproject/services/interfaces/ColumnService.java
+++ code/src/main/java/com/myproject/services/interfaces/ColumnService.java
@@ -1,11 +1,12 @@
 package com.myproject.services.interfaces;
 
-import com.myproject.models.dtos.BulkUpdateColumnCountsRequest;
-import com.myproject.models.dtos.BulkUpdateColumnCountsResponse;
-import com.myproject.models.dtos.ColumnStatsResponse;
+import com.myproject.models.dtos.*;
 
 public interface ColumnService {
+
     ColumnStatsResponse getColumnStats(String columnId);
+
     BulkUpdateColumnCountsResponse bulkUpdateColumnCounts(BulkUpdateColumnCountsRequest request);
+
     void updateColumnCounts(String oldColumnId, String newColumnId);
 }
--- code/src/main/java/com/myproject/services/interfaces/TaskService.java
+++ code/src/main/java/com/myproject/services/interfaces/TaskService.java
@@ -4,13 +4,24 @@ import com.myproject.models.dtos.*;
 import java.util.List;
 
 public interface TaskService {
+
     TaskResponse createTask(TaskCreateRequest request);
-    TaskResponse getTaskById(Long taskId);
-    PagedTaskResponse getUserTasks(Long userId, int page, int size);
-    TaskCountResponse getTaskCount(Long userId);
+
     TaskResponse updateTask(Long taskId, TaskUpdateRequest request);
+
+    TaskResponse getTaskById(Long taskId);
+
+    List<TaskResponse> listTasks();
+
     void deleteTask(Long taskId);
+
+    PagedTaskResponse getUserTasks(Long userId, int page, int size);
+
+    TaskCountResponse getTaskCount(Long userId);
+
     BulkTaskResponse bulkCreateTasks(List<TaskCreateRequest> requests);
+
     UpdateTaskStatusResponse updateTaskStatus(String taskId, UpdateTaskStatusRequest request);
+
     TaskDetailsResponse getTaskDetails(String taskId);
 }
